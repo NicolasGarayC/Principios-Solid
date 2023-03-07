36ee3a245c5cb03c946f2077d52451c7e30cbb6f
lvavila80 <124851074+lvavila80@users.noreply.github.com>
2023-03-07 01:16:19 -0500

    Crear objetos de las clases Factura, Deduccion e Iva dentro del método main y llamar a los métodos correspondientes

--- InversionDeDependencias/src/main/java/com/mycompany/inversiondedependencias/Deduccion.java
+++ InversionDeDependencias/src/main/java/com/mycompany/inversiondedependencias/Deduccion.java
@@ -1,6 +1,6 @@
 package com.mycompany.inversiondedependencias;
 
-public class Deduccion implements Impuesto {
+public class Deduccion implements Impuesto{
 
     public float calcularImpuesto(float importe) {
         return importe;
