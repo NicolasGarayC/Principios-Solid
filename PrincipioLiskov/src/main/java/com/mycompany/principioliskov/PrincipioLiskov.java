b90f1a16077e900281ce2c55ac9c559dd3fe5301
lvavila80 <124851074+lvavila80@users.noreply.github.com>
2023-03-07 01:41:10 -0500

    Implementación del Main mediante asignación de instancias

--- PrincipioLiskov/src/main/java/com/mycompany/principioliskov/PrincipioLiskov.java
+++ PrincipioLiskov/src/main/java/com/mycompany/principioliskov/PrincipioLiskov.java
@@ -7,9 +7,14 @@ public class PrincipioLiskov {
 
     public static void main(String[] args) {
         List<RecursoPersistente> recursos = new ArrayList<>();
+        recursos.add(new ConfiguracionSistema());
+        recursos.add(new ConfiguracionUsuario());
         recursos.add(new ConfiguracionHoraria());
-//        Configuracion configuracion<> = new Configuracion();
-//        configuracion.cargarConfiguracion();
-//        configuracion.salvarConfiguracion();
+
+        Configuracion configuracion = new Configuracion(recursos);
+        configuracion.cargarConfiguracion();
+        configuracion.salvarConfiguracion();
     }
 }
+
+
